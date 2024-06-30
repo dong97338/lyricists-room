57af1a828b609b4efdb9075b7357a404a04a956a
dong97338 <dong97338@gmail.com>
2024-06-30 17:07:49 +0900

    Update global CSS styles and layout component

--- app/buffer.js
+++ app/buffer.js
@@ -7,7 +7,7 @@ export default function Home() {
   const router = useRouter();
 
   const handleStart = () => {
-    router.push('graph');
+    router.push('page2.js');
   };
 
   return (
--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,4 @@
-import { Inter } from "next/font/google";
+/*import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -12,6 +12,19 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
+*/
+// app/layout.js
+import './globals.css';
+
+export default function Layout({ children }) {
+  return (
+    <html lang="en">
+      <body className="min-h-screen flex justify-center items-center">
+        {children}
+      </body>
     </html>
   );
 }
