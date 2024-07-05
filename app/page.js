6b7b3fabcc00881e31e8a7fe58cb637d0aadd8c7
KwakChaehun <kwak3006@gmail.com>
2024-07-05 17:45:23 +0900

    프롬프트 및 옵션 추가

--- app/page.js
+++ app/page.js
@@ -10,9 +10,18 @@ export default () => {
       <h1 className="text-5xl font-bold mb-8">Lyricist's Room</h1>
       <input type="text" placeholder="WRITE YOUR TOPIC" className="w-80 p-3 mb-4 border rounded-md text-lg" value={topic} onChange={e => setTopic(e.target.value)} />
       <select className="w-80 p-3 mb-4 border rounded-md text-lg">
-        <option>CHOOSE YOUR MUSIC GENRE</option>
-        <option>발라드</option>
-        <option>인디뮤직</option>
+        <option>CHOOSE YOUR MUSIC 분위기</option>
+        <option>그리움</option>
+        <option>당당함</option>
+        <option>불안함</option>
+        <option>설렘</option>
+        <option>슬픔</option>
+        <option>신남</option>
+        <option>외로움</option>
+        <option>우울함</option>
+        <option>평화로움</option>
+        <option>화남</option>
+        <option>희망찬</option>
         {/* 원하는 장르를 추가 */}
       </select>
       <button className="w-80 p-3 bg-gray-400 rounded-md text-lg" onClick={() => router.push(`graph?topic=${encodeURIComponent(topic)}`)}>
