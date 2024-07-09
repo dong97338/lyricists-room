3b4ed3ebf9a8b5cd7a8bdb371495c83de4f219f8
dong97338 <dong97338@gmail.com>
2024-07-10 00:59:50 +0900

    fix: 곡 분위기 -> 가사 분위기

--- app/page.js
+++ app/page.js
@@ -13,7 +13,7 @@ export default () => {
       <input type="text" placeholder="WRITE YOUR TOPIC" className="mb-4" value={topic} onChange={e => setTopic(e.target.value)} />
       <input type="text" placeholder="WRITE YOUR KEY MESSAGE" className="mb-4" value={key} onChange={e => setKey(e.target.value)} />
       <select value={mood} className="mb-4" onChange={e => setMood(e.target.value)}>
-        <option>곡 분위기를 선택해주세요!</option>
+        <option>가사 분위기를 선택해주세요!</option>
         {['그리움', '당당함', '불안함', '설렘', '슬픔', '신남', '외로움', '우울함', '평화로움', '화남', '희망찬'].map(mood => (
           <option>{mood}</option>
         ))}
