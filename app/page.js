b8a304f724d9303145f6a6770be383f093e26df3
dong97338 <dong97338@gmail.com>
2024-07-10 15:28:44 +0900

    Update styling in globals.css and page.js

--- app/page.js
+++ app/page.js
@@ -9,10 +9,10 @@ export default () => {
 
   return (
     <>
-      <h1 className="mb-8 text-5xl font-bold">Lyricist's Room</h1>
-      <input type="text" placeholder="키워드를 입력해주세요.. 한 단어일수록 좋아요!" className="mb-4" value={topic} onChange={e => setTopic(e.target.value)} />
-      <input type="text" placeholder="키메시지를 입력해주세요!" className="mb-4" value={key} onChange={e => setKey(e.target.value)} />
-      <select value={mood} className="mb-4" onChange={e => setMood(e.target.value)}>
+      <h1 className="mb-4 text-5xl font-bold">Lyricist's Room</h1>
+      <input type="text" placeholder="키워드를 입력해주세요.. 한 단어일수록 좋아요!" value={topic} onChange={e => setTopic(e.target.value)} />
+      <input type="text" placeholder="키메시지를 입력해주세요!" value={key} onChange={e => setKey(e.target.value)} />
+      <select value={mood} onChange={e => setMood(e.target.value)}>
         <option>가사 분위기를 선택해주세요!</option>
         {['그리움', '당당함', '불안함', '설렘', '슬픔', '신남', '외로움', '우울함', '평화로움', '화남', '희망찬'].map(mood => (
           <option>{mood}</option>
