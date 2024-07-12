a4ff9b903cf78c4a327edbff848fb685ee6eb3ea
KwakChaehun <kwak3006@gmail.com>
2024-07-12 18:00:46 +0900

    사이드바 홈 추가

--- app/graph/page.js
+++ app/graph/page.js
@@ -4,10 +4,13 @@ import {useSearchParams} from 'next/navigation'
 import * as d3 from 'd3'
 import OpenAI from 'openai'
 import dotenv from 'dotenv'
+import { useRouter } from 'next/navigation'
+
 dotenv.config()
 
 function Graph() {
   const searchParams = useSearchParams()
+  const router = useRouter()
   const [graph, setGraph] = useState({nodes: [{id: 1, name: searchParams.get('topic') || 'No topic', fx: 920,fy: 400}], links: []})
   const [sentence, setSentence] = useState('')
   const [loadingNode, setLoadingNode] = useState(null)
@@ -30,128 +33,101 @@ function Graph() {
   useEffect(() => {
     const svg = d3.select(svgRef.current)
     svg.selectAll('*').remove()
-  
+
     // Add zoom functionality
-  
     const containerGroup = svg.append('g')  // Add a group to apply zoom transformations
-  
+
     const linkGroup = containerGroup.append('g').attr('class', 'links')
     const nodeGroup = containerGroup.append('g').attr('class', 'nodes')
-  
 
     const simulation = d3
-      .forceSimulation(graph.nodes)
-      .force(
-        'link',
-        d3
-          .forceLink(graph.links)
-          .id(d => d.id)
-          .distance(50)
-          .strength(1)
-      )
-      .force('charge', d3.forceManyBody().strength(-500))
-      .force('collision', d3.forceCollide().radius(40))
-      .on('tick', () => {
-        linkGroup
-          .selectAll('line')
-          .data(graph.links)
-          .join('line')
-          .attr('x1', d => d.source.x)
-          .attr('y1', d => d.source.y)
-          .attr('x2', d => d.target.x)
-          .attr('y2', d => d.target.y)
-          .attr('stroke', '#999')
-          .attr('stroke-width', 2)
-        nodeGroup
-          .selectAll('g')
-          .data(graph.nodes)
-          .join(
-            enter => {
-              const nodeEnter = enter
-                .append('g')
-                .attr('class', 'node')
-                .on('click', (e, d) => handleNodeClick(e, d))
-                .call(
-                  d3
-                    .drag()
-                    .on('start', (e, d) => {
-                      if (!e.active) simulation.alphaTarget(0.3).restart()
-                      d.fx = d.x
-                      d.fy = d.y
-                    })
-                    .on('drag', (e, d) => {
-                      d.fx = e.x
-                      d.fy = e.y
-                    })
-                    .on('end', (e, d) => {
-                      if (!e.active) simulation.alphaTarget(0)
-                      if (d.id !== 1) {
-                        d.fx = null
-                        d.fy = null
-                      }
-                    })
-                )
-                .on('mouseenter', function (e, d) {
-                  d3.select(this).select('circle').attr('fill', '#ffa500') // Change the node color on hover
-                  svg.style('cursor', 'pointer') // Change cursor to pointer
+  .forceSimulation(graph.nodes)
+  .force(
+    'link',
+    d3
+      .forceLink(graph.links)
+      .id(d => d.id)
+      .distance(70)
+      .strength(0.7) // 링크 강도를 낮춰서 노드가 덜 흔들리게 합니다.
+  )
+  .force('charge', d3.forceManyBody().strength(-30)) // 노드 간의 반발력을 줄입니다.
+  .force('collision', d3.forceCollide().radius(40).strength(0.7)) // 충돌 강도를 조정하여 부드럽게 합니다.
+  .alphaDecay(0.05) // 시뮬레이션의 알파 감소율을 높여 시뮬레이션이 더 빨리 안정되도록 합니다.
+  .on('tick', () => {
+    linkGroup
+      .selectAll('line')
+      .data(graph.links)
+      .join('line')
+      .attr('x1', d => d.source.x)
+      .attr('y1', d => d.source.y)
+      .attr('x2', d => d.target.x)
+      .attr('y2', d => d.target.y)
+      .attr('stroke', '#999')
+      .attr('stroke-width', 2)
+    nodeGroup
+      .selectAll('g')
+      .data(graph.nodes)
+      .join(
+        enter => {
+          const nodeEnter = enter
+            .append('g')
+            .attr('class', 'node')
+            .on('click', (e, d) => handleNodeClick(e, d))
+            .call(
+              d3
+                .drag()
+                .on('start', (e, d) => {
+                  if (!e.active) simulation.alphaTarget(0.3).restart()
+                  d.fx = d.x
+                  d.fy = d.y
                 })
-                .on('mouseleave', function (e, d) {
-                  d3.select(this).select('circle').attr('fill', '#d9d9d9') // Revert the node color
-                  svg.style('cursor', 'default') // Revert cursor to default
+                .on('drag', (e, d) => {
+                  d.fx = e.x
+                  d.fy = e.y
                 })
-              nodeEnter.append('circle').attr('r', 20).attr('fill', '#d9d9d9')
-              nodeEnter
-                .append('text')
-                .attr('dy', 4)
-                .attr('x', -10)
-                .attr('font-size', 12)
-                .text(d => d.name)
-
-              nodeEnter.each(function (d) {
-                if (loadingNode === d.id) {
-                  d3.select(this)
-                    .append('svg')
-                    .attr('x', -35)
-                    .attr('y', -35)
-                    .attr('width', 70)
-                    .attr('height', 70)
-                    .attr('viewBox', '0 0 200 200')
-                    .html(
-                      "<radialGradient id='a10' cx='.66' fx='.66' cy='.3125' fy='.3125' gradientTransform='scale(1.5)'><stop offset='0' stop-color='#000000'></stop><stop offset='.3' stop-color='#000000' stop-opacity='.9'></stop><stop offset='.6' stop-color='#000000' stop-opacity='.6'></stop><stop offset='.8' stop-color='#000000' stop-opacity='.3'></stop><stop offset='1' stop-color='#000000' stop-opacity='0'></stop></radialGradient><circle transform-origin='center' fill='none' stroke='url(#a10)' stroke-width='15' stroke-linecap='round' stroke-dasharray='200 1000' stroke-dashoffset='0' cx='100' cy='100' r='70'><animateTransform type='rotate' attributeName='transform' calcMode='spline' dur='2' values='360;0' keyTimes='0;1' keySplines='0 0 1 1' repeatCount='indefinite'></animateTransform></circle><circle transform-origin='center' fill='none' opacity='.2' stroke='#000000' stroke-width='15' stroke-linecap='round' cx='100' cy='100' r='70'></circle>"
-                    )
-                }
-              })
-              .on('mouseleave', function (e, d) {
-                d3.select(this).select('circle').attr('fill', '#d9d9d9') // Revert the node color
-                svg.style('cursor', 'default') // Revert cursor to default
-              })
-            nodeEnter.append('circle').attr('r', 30).attr('fill', '#d9d9d9')
-            nodeEnter
-              .append('text')
-              .attr('dy', 4)
-              .attr('x', 0)  // 중앙 정렬을 위해 x 속성을 0으로 설정
-              .attr('text-anchor', 'middle')  // 중앙 정렬을 위해 text-anchor 속성을 middle로 설정
-              .attr('font-size', 12)
-              .text(d => d.name)
-            nodeEnter.each(function (d) {
-              if (loadingNode === d.id) {
-                d3.select(this)
-                  .append('svg')
-                  .attr('x', -35)
-                  .attr('y', -35)
-                  .attr('width', 70)
-                  .attr('height', 70)
-                  .attr('viewBox', '0 0 200 200')
-                  .html(
-                    "<radialGradient id='a10' cx='.66' fx='.66' cy='.3125' fy='.3125' gradientTransform='scale(1.5)'><stop offset='0' stop-color='#000000'></stop><stop offset='.3' stop-color='#000000' stop-opacity='.9'></stop><stop offset='.6' stop-color='#000000' stop-opacity='.6'></stop><stop offset='.8' stop-color='#000000' stop-opacity='.3'></stop><stop offset='1' stop-color='#000000' stop-opacity='0'></stop></radialGradient><circle transform-origin='center' fill='none' stroke='url(#a10)' stroke-width='15' stroke-linecap='round' stroke-dasharray='200 1000' stroke-dashoffset='0' cx='100' cy='100' r='70'><animateTransform type='rotate' attributeName='transform' calcMode='spline' dur='2' values='360;0' keyTimes='0;1' keySplines='0 0 1 1' repeatCount='indefinite'></animateTransform></circle><circle transform-origin='center' fill='none' opacity='.2' stroke='#000000' stroke-width='15' stroke-linecap='round' cx='100' cy='100' r='70'></circle>"
-                  )
-              }
+                .on('end', (e, d) => {
+                  if (!e.active) simulation.alphaTarget(0)
+                  if (d.id !== 1) {
+                    d.fx = null
+                    d.fy = null
+                  }
+                })
+            )
+            .on('mouseenter', function (e, d) {
+              d3.select(this).select('circle').attr('fill', '#ffa500') // Change the node color on hover
+              svg.style('cursor', 'pointer') // Change cursor to pointer
             })
-            return nodeEnter
+            .on('mouseleave', function (e, d) {
+              d3.select(this).select('circle').attr('fill', '#d9d9d9') // Revert the node color
+              svg.style('cursor', 'default') // Revert cursor to default
+            })
+          nodeEnter.append('circle').attr('r', 30).attr('fill', '#d9d9d9')
+          nodeEnter
+            .append('text')
+            .attr('dy', 4)
+            .attr('x', 0)  // 중앙 정렬을 위해 x 속성을 0으로 설정
+            .attr('text-anchor', 'middle')  // 중앙 정렬을 위해 text-anchor 속성을 middle로 설정
+            .attr('font-size', 12)
+            .text(d => d.name)
+
+          nodeEnter.each(function (d) {
+            if (loadingNode === d.id) {
+              d3.select(this)
+                .append('svg')
+                .attr('x', -35)
+                .attr('y', -35)
+                .attr('width', 70)
+                .attr('height', 70)
+                .attr('viewBox', '0 0 200 200')
+                .html(
+                  "<radialGradient id='a10' cx='.66' fx='.66' cy='.3125' fy='.3125' gradientTransform='scale(1.5)'><stop offset='0' stop-color='#000000'></stop><stop offset='.3' stop-color='#000000' stop-opacity='.9'></stop><stop offset='.6' stop-color='#000000' stop-opacity='.6'></stop><stop offset='.8' stop-color='#000000' stop-opacity='.3'></stop><stop offset='1' stop-color='#000000' stop-opacity='0'></stop></radialGradient><circle transform-origin='center' fill='none' stroke='url(#a10)' stroke-width='15' stroke-linecap='round' stroke-dasharray='200 1000' stroke-dashoffset='0' cx='100' cy='100' r='70'><animateTransform type='rotate' attributeName='transform' calcMode='spline' dur='2' values='360;0' keyTimes='0;1' keySplines='0 0 1 1' repeatCount='indefinite'></animateTransform></circle><circle transform-origin='center' fill='none' opacity='.2' stroke='#000000' stroke-width='15' stroke-linecap='round' cx='100' cy='100' r='70'></circle>"
+                )
+            }
           })
-          .attr('transform', d => `translate(${d.x},${d.y})`)
-      })
-    //const containerGroup = svg.append("g");
+          return nodeEnter
+        })
+      .attr('transform', d => `translate(${d.x},${d.y})`)
+  })
 
     // Add zoom and pan functionality
     const zoom = d3.zoom()
@@ -172,10 +148,39 @@ function Graph() {
     simulation.alpha(1).restart()
   }, [graph, loadingNode])
 
+  const [sidebarOpen, setSidebarOpen] = useState(false)
+  const toggleSidebar = () => {
+    setSidebarOpen(!sidebarOpen)
+    if (!sidebarOpen) {
+      document.body.style.overflow = 'hidden'
+    } else {
+      document.body.style.overflow = 'auto'
+    }
+  }
+  const handleHomeClick = () => {
+    router.push('/')
+    console.log('Home button clicked')
+  }
+
   return (
-    <>
+  <div style={{ display: 'flex', height: '100vh', overflow: 'hidden' }}>
+    {sidebarOpen && (
+      <div style={{ width: '250px', background: '#f0f0f0', padding: '20px', position: 'fixed', left: 0, top: 0, bottom: 0 }}>
+        <h2>Sidebar</h2>
+        <p>Some content here</p>
+      </div>
+    )}
+    <div style={{ flex: 1, display: 'flex', flexDirection: 'column', alignItems: 'center', marginLeft: sidebarOpen ? '500px' : '0' }}>
+      <div style={{ display: 'flex', justifyContent: 'space-between', width: '100%', padding: '10px' }}>
+        <button onClick={toggleSidebar} style={{ padding: '10px', fontSize: '16px' }}>
+          {sidebarOpen ? 'Close' : 'History'}
+        </button>
+        <button onClick={handleHomeClick} style={{ padding: '10px', fontSize: '16px', marginRight: sidebarOpen ? '250px' : '0'  }}>
+          Home
+        </button>
+      </div>
       <svg ref={svgRef} width="1840" height="800"></svg>
-      <div className="flex w-full items-center justify-center" style={{ marginTop: '50px', marginBottom: '0px' }}>
+      <div className="flex w-full items-center justify-center" style={{ marginTop: '0px', marginBottom: '0px' }}>
         <input
           type="text"
           placeholder="MAKE A SENTENCE USING THE CHOSEN WORD"
@@ -191,8 +196,9 @@ function Graph() {
           MAKE
         </button>
       </div>
-    </>
-  )
+    </div>
+  </div>
+)
 }
 
 export default () => (
--- app/page.js
+++ app/page.js
@@ -10,15 +10,35 @@ export default () => {
   return (
     <>
       <h1 className="mb-4 text-5xl font-bold">Lyricist's Room</h1>
-      <input type="text" placeholder="키워드를 입력해주세요.. 한 단어일수록 좋아요!" value={topic} onChange={e => setTopic(e.target.value)} />
-      <input type="text" placeholder="키메시지를 입력해주세요!" value={key} onChange={e => setKey(e.target.value)} />
-      <select value={mood} onChange={e => setMood(e.target.value)}>
+      <input
+        type="text"
+        placeholder="키워드를 입력해주세요.. 한 단어일수록 좋아요!"
+        value={topic}
+        onChange={e => setTopic(e.target.value)}
+        style={{ width: '450px', padding: '10px'}} // 스타일 추가
+      />
+      <input
+        type="text"
+        placeholder="키메시지를 입력해주세요!"
+        value={key}
+        onChange={e => setKey(e.target.value)}
+        style={{ width: '450px', padding: '10px'}} // 스타일 추가
+      />
+      <select
+        value={mood}
+        onChange={e => setMood(e.target.value)}
+        style={{ width: '450px', padding: '10px'}} // 스타일 추가
+      >
         <option>가사 분위기를 선택해주세요!</option>
         {['그리움', '당당함', '불안함', '설렘', '슬픔', '신남', '외로움', '우울함', '평화로움', '화남', '희망찬'].map(mood => (
-          <option>{mood}</option>
+          <option key={mood}>{mood}</option>
         ))}
       </select>
-      <Link className="w-80 rounded-md bg-gray-400 p-3 text-center text-lg" href={`graph?${new URLSearchParams({topic, key, mood}).toString()}`}>
+      <Link
+        href={`graph?${new URLSearchParams({topic, key, mood}).toString()}`}
+        className="rounded-md bg-gray-400 text-center text-xl"
+        style={{ display: 'inline-block', width: '200px', padding: '10px'}} // 스타일 추가
+      >
         start
       </Link>
     </>
