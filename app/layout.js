23ef6d5c99bfef935a24a3d4f0422a922e894518
dong97338 <dong97338@gmail.com>
2024-07-14 21:57:05 +0900

    Refactor styling in globals.css and layout.js

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,6 @@ import './globals.css'
 
 export default ({children}) => (
   <html lang="en">
-    <body className="flex min-h-screen flex-col items-center justify-center space-y-4">{children}</body>
+    <body className="flex min-h-screen flex-col items-center justify-center space-y-4 bg-gray-100">{children}</body>
   </html>
 )
--- app/page.js
+++ app/page.js
@@ -10,37 +10,15 @@ export default () => {
   return (
     <>
       <h1 className="mb-4 text-5xl font-bold">Lyricist's Room</h1>
-      <input
-        type="text"
-        placeholder="키워드를 입력해주세요.. 한 단어일수록 좋아요!"
-        value={topic}
-        onChange={e => setTopic(e.target.value)}
-        style={{ width: '450px', padding: '10px'}} // 스타일 추가
-      />
-      <input
-        type="text"
-        placeholder="키메시지를 입력해주세요!"
-        value={key}
-        onChange={e => setKey(e.target.value)}
-        style={{ width: '450px', padding: '10px'}} // 스타일 추가
-      />
-      <select
-        value={mood}
-        onChange={e => setMood(e.target.value)}
-        style={{ width: '450px', padding: '10px'}} // 스타일 추가
-      >
+      <input type="text" placeholder="키워드를 입력해주세요.. 한 단어일수록 좋아요!" value={topic} onChange={e => setTopic(e.target.value)} />
+      <input type="text" placeholder="키메시지를 입력해주세요!" value={key} onChange={e => setKey(e.target.value)} />
+      <select value={mood} onChange={e => setMood(e.target.value)}>
         <option>가사 분위기를 선택해주세요!</option>
         {['그리움', '당당함', '불안함', '설렘', '슬픔', '신남', '외로움', '우울함', '평화로움', '화남', '희망찬'].map(mood => (
           <option key={mood}>{mood}</option>
         ))}
       </select>
-      
-      <Link
-        
-        href={`graph?${new URLSearchParams({topic, key, mood}).toString()}`}
-        className="rounded-md bg-gray-400 text-center text-xl"
-        style={{ display: 'inline-block', width: '200px', padding: '10px'}} // 스타일 추가
-      >
+      <Link className="w-[200px] rounded-md bg-gray-400 p-2.5 text-center text-xl" href={`graph?${new URLSearchParams({topic, key, mood}).toString()}`}>
         start
       </Link>
     </>
