66591edcdab370c1b263a687c3f1f103cf82890c
KwakChaehun <kwak3006@gmail.com>
2024-07-16 01:36:22 +0900

    ㅇㅇㅇ

--- app/page.js
+++ app/page.js
@@ -10,9 +10,17 @@ export default () => {
   return (
     <>
       <h1 className="mb-4 text-5xl font-bold">Lyricist's Room</h1>
-      <input placeholder="키워드를 입력해주세요.. 한 단어일수록 좋아요!" value={topic} onChange={e => setTopic(e.target.value)} />
-      <input placeholder="키메시지를 입력해주세요!" value={key} onChange={e => setKey(e.target.value)} />
-      <select value={mood} onChange={e => setMood(e.target.value)}>
+      <input 
+        placeholder="키워드를 입력해주세요.. 한 단어일수록 좋아요!" value={topic} onChange={e => setTopic(e.target.value)}
+        className="w-full md:w-[450px] h-14 p-4 text-lg border rounded-lg"
+      />
+      <input
+        placeholder="키메시지를 입력해주세요!" value={key} onChange={e => setKey(e.target.value)}
+        className="w-full md:w-[450px] h-14 p-4 text-lg border rounded-lg"
+      />
+      <select value={mood} onChange={e => setMood(e.target.value)}
+        className="w-full md:w-[450px] h-14 p-4 text-lg border rounded-lg"
+        >
         <option>가사 분위기를 선택해주세요!</option>
         {['그리움', '당당함', '불안함', '설렘', '슬픔', '신남', '외로움', '우울함', '평화로움', '화남', '희망찬'].map(mood => (
           <option key={mood}>{mood}</option>
