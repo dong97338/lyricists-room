acd35ef1e3155f8712ae58ae0adb986af864b9bd
dong97338 <dong97338@gmail.com>
2024-07-16 03:57:11 +0900

    Refactor page.js to update select options and styles

--- app/page.js
+++ app/page.js
@@ -13,12 +13,10 @@ export default () => {
       <h1 className="mb-4 text-3xl font-bold md:text-5xl">Lyricist's Room</h1>
       <input placeholder="키워드를 입력해주세요.. 한 단어일수록 좋아요!" value={topic} onChange={e => setTopic(e.target.value)} />
       <input placeholder="키메시지를 입력해주세요!" value={key} onChange={e => setKey(e.target.value)} />
-      <select value={mood} onChange={e => setMood(e.target.value)}>
-        <option aria-label="Select">가사 분위기를 선택해주세요!</option>
+      <select aria-label="분위기" value={mood} onChange={e => setMood(e.target.value)}>
+        <option>가사 분위기를 선택해주세요!</option>
         {['그리움', '당당함', '불안함', '설렘', '슬픔', '신남', '외로움', '우울함', '평화로움', '화남', '희망찬'].map(mood => (
-          <option aria-label={mood} key={mood}>
-            {mood}
-          </option>
+          <option key={mood}>{mood}</option>
         ))}
       </select>
       <button className="w-[200px] rounded-md bg-gray-400 p-2.5 text-center text-xl" onClick={() => `graph?${new URLSearchParams({topic, key, mood}).toString()}`}>
