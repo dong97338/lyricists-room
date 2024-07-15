ebce031a8c37f8c2cf60f6d3be4cb5ca9a8c7f3d
dong97338 <dong97338@gmail.com>
2024-07-16 02:01:15 +0900

    Add DefaultSeo component to layout.js and create next-seo.config.js

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,14 @@
 import './globals.css'
 import {Analytics} from '@vercel/analytics/react'
 import {GoogleAnalytics} from '@next/third-parties/google'
+import {DefaultSeo} from 'next-seo'
+import SEO from '../next-seo.config'
 
 export default ({children}) => (
   <html lang="en">
+    <head>
+      <DefaultSeo {...SEO} />
+    </head>
     <body className="flex min-h-screen flex-col items-center justify-center space-y-4 bg-gray-100">
       {children}
       <Analytics />
